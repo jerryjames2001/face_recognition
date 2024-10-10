86f748c75bcc81a9773fbd6c47b17b4441bdbc75
JERRY JAMES <jerryjamesedavazhickal@gmail.com>
2024-10-10 06:25:12 +0530

    removed eslint config

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -2,16 +2,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Home from './pages/Home/home'
 import About from './pages/Aboutus/Aboutus'
-import Nav_before from './pages/Nav/nav1'
+import Nav1 from './pages/Nav/nav1'
 import Nav2 from './pages/Nav/nav2'
 import Login from './pages/LoginRegister/Login'
 import Register from './pages/LoginRegister/Register'
-import toast, { Toaster } from 'react-hot-toast'
+import { Toaster } from 'react-hot-toast'
 function App () {
   return (
     <>
       <BrowserRouter>
-        <Nav_before />{/* importing nav components */}
+        <Nav1 />{/* importing nav components */}
         {/* <Nav2 /> */}
         <Routes>
           <Route path='/' element={<Home />} />
